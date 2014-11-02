7c6dd66a649bd576eea5a75e3121c478688e3b46
Benjamin Pearce <hawk.git@bcpearce.com>
2014-11-02 15:06:04 -0500

    added method for seeding airports using data from ourairports.com

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+['csv', 'open-uri'].each { |lib| require lib }
+
+Airport.delete_all
+
+airport_data = open("http://ourairports.com/data/airports.csv")
+
+CSV.parse(airport_data, headers:true) do |row|
+  Airport.create(code:row['iata_code'])
+  if row['iata_code']
+    $stdout.flush
+    print "Created Airport #{row['iata_code']}\r"
+  end
+end
