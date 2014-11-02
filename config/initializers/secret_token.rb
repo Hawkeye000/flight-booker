5bffb76fc538f416527c0ae9d408781859bbeafb
Benjamin Pearce <hawk.git@bcpearce.com>
2014-11-02 13:20:37 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FlightBooker::Application.config.secret_key_base = '679277d10f43349d4af24a2aff318568b185a0b46d9d16265d25f3e049a55ea28a252e3c0840263f0f928cdf4fbdc44e2a5666147565d1e3486c36941a4318a6'
