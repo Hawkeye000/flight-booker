b038dec6889695d7b7fe462905036779ae2cf743
Benjamin Pearce <hawk.git@bcpearce.com>
2014-11-04 23:16:32 -0500

    added date return function for Flight class

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ActiveRecord::Base
   belongs_to :to_airport, class_name:"Airport"
   belongs_to :from_airport, class_name:"Airport"
+
+  def self.all_dates
+    (self.all.collect { |x| x.datetime.to_date } ).uniq.sort
+  end
 end
