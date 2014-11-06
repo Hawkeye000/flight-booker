9e9089167a5cf2c1ad9147f3b783262273de6536
Benjamin Pearce <hawk.git@bcpearce.com>
2014-11-05 23:52:16 -0500

    added booking id to passenger

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106010058) do
+ActiveRecord::Schema.define(version: 20141106044219) do
 
   create_table "airports", force: true do |t|
     t.string   "code"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20141106010058) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "booking_id"
   end
 
 end
