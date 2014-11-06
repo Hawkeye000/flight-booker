2d278d12eacec61996fb1cbd226dab863649d600
Benjamin Pearce <hawk.git@bcpearce.com>
2014-11-05 23:18:28 -0500

    added routes for bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 FlightBooker::Application.routes.draw do
 
-  root 'flights#index'
   resources :flights, only: [:index]
   resources :bookings, only: [:new]
+
+  root 'flights#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
