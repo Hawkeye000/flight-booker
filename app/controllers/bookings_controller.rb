2fddd0a5594f113ee1c5949fdc44c4aac0b30932
Benjamin Pearce <hawk.git@bcpearce.com>
2014-11-05 23:18:58 -0500

    added bookings#new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,15 @@
 class BookingsController < ApplicationController
 
+  def index
+  end
+
   def new
     @booking = Booking.new
+    @flight = Flight.find_by_id(params[:flight_id])
+    @passengers = []
+    params[:passenger_quantity].to_i.times do
+       @passengers << Passenger.new
+    end
   end
 
 end
